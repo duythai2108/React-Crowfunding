d8696f3e9d29d05775b63594251d72c21f867f3c
duythai2108 <thaiduy21082000@gmail.com>
2023-08-11 10:43:33 +0700

    SearchComponents

--- src/layout/LayoutDashboard.js
+++ src/layout/LayoutDashboard.js
@@ -1,3 +1,4 @@
+import Overlay from "components/common/Overlay";
 import DashboardSidebar from "modules/dashboard/DashboardSidebar";
 import DashboardTopbar from "modules/dashboard/DashboardTopbar";
 import React from "react";
@@ -5,6 +6,7 @@ import React from "react";
 const LayoutDashboard = ({ children }) => {
   return (
     <div className="p-10 bg-lite min-h-screen">
+      <Overlay></Overlay>
       <DashboardTopbar></DashboardTopbar>
       <div className="flex gap-x-10 items-start">
         <DashboardSidebar></DashboardSidebar>
